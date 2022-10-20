49863c80f387282c8c6fb28e84f4297bc8832168
Fredy Acevedo <chicor511@hotmail.com>
2022-10-19 23:40:30 -0500

    creacion de algoritmos

---
+++ index.js
@@ -0,0 +1,6 @@
+//mi primera variable
+
+let name = prompt('Introduce tu nombre')
+let age =  parseInt( prompt('Introduce tu edad'))
+
+console.log (`Hola ${name} tienes ${age} años y el año que viene tendras ${age+1} años`);
