7e40b88a92c6c477d4880a18fd4bdcc10ab06dc5
Fredy Acevedo <chicor511@hotmail.com>
2022-10-24 23:43:02 -0500

    bucleFor

--- index.js
+++ index.js
@@ -36,3 +36,21 @@ while (seguirComprando===true){
 
 alert(`El total de tu compra es ${totalCompra}`)
 console.log(`El total de la compra de ${name} es de ${totalCompra}`);
+
+let num = parseInt(prompt('Introduce un numero no mayor a 20'))
+
+console.log('Inicio de Ciclo');
+
+if(num<=20){
+for( let i =1; i<=num; i++){
+    if(i%2 ===0){
+        console.log(`${i} es par`);
+    }else{
+        console.log(`${i} es impar`);
+    }
+}
+}else{
+    alert('El numero ingresado es mayor a 20')
+}
+console.log('Fin del Ciclo');
+alert('Gracias por su compra')
